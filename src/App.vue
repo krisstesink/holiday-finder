aea8e50e285431f06ca63122d0af211afecaf7ea
ktesink <krisstesink1@gmail.com>
2024-12-13 22:43:39 +0100

    implement dynamic holiday fetching based on selected country

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <CountrySearchComponent/>
+  <CountrySearchComponent @itemSelected="handleItemSelected"/>
+  <ListComponent :items="holidays"/>
 </template>
 
 <script>
 import CountrySearchComponent from './components/CountrySearchComponent.vue'
+import ListComponent from './components/ListComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    CountrySearchComponent
+    CountrySearchComponent,
+    ListComponent,
+  },
+
+  methods: {
+    async handleItemSelected(item) {
+      console.log("assdfads")
+      try {
+        const response = await fetch("https://date.nager.at/api/v3/PublicHolidays/2025/" + item.countryCode);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        this.holidays = await response.json();
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+
+    }
+  },
+
+  data() {
+    return {
+      
+  holidays: null,
+
+    }
   }
 }
 </script>
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+
+        props: {
+            items: {
+                type: Array,
+                default: () => null, 
+            }
+
+        },
+    }
+</script>
+
+<template>
+    <section class="list-wrapper">
+        <table>
+            <tr v-for="(item, index) in items" :key="index">
+                <td>{{ item }}</td>
+
+            </tr>
+        </table>
+
+
+    </section>
+</template>
