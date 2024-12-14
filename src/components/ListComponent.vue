3ebe69f4f251d0f3f941fafa25fc717e033a1d23
ktesink <krisstesink1@gmail.com>
2024-12-14 14:29:24 +0100

    add table for displaying holidays

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -1,25 +1,51 @@
-<script>
-    export default {
-
-        props: {
-            items: {
-                type: Array,
-                default: () => null, 
-            }
-
-        },
-    }
-</script>
-
 <template>
-    <section class="list-wrapper">
-        <table>
-            <tr v-for="(item, index) in items" :key="index">
-                <td>{{ item }}</td>
-
-            </tr>
+    <section class="list-wrapper container mt-4">
+        <h2 class="text-center mb-4">Holiday List</h2>
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th style="width: 50%;">Date</th>
+                    <th style="width: 50%;">Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in items" :key="index">
+                    <td>{{ item.date }}</td>
+                    <td>{{ item.name }}</td>
+                </tr>
+            </tbody>
         </table>
-
-
     </section>
 </template>
+
+<script>
+export default {
+    props: {
+        items: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
+</script>
+
+<style>
+.list-wrapper {
+    padding: 20px;
+}
+.table {
+    width: 100%;
+    margin: auto;
+    text-align: left;
+    table-layout: fixed; 
+}
+.text-center {
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+th, td {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+</style>
